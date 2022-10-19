846c934ef01fb199c59a4f61d2aeb5a4cbebd3d6
vy923 <v.v.yotov@pm.me>
2022-10-19 18:37:56 +1300

    generic
    v0.1.0

--- src/eigsol.m
+++ src/eigsol.m
@@ -1,24 +1,19 @@
-%{
--------------------------------------
-    Vladimir V. Yotov
-    Te Pūnaha Ātea Space Institute
-    University of Auckland
-
-    Version: 16.12.2021
--------------------------------------
-
-NOTES
-	Eigensolution
-    -- use switch nargin instead
-%}
+%  ------------------------------------------------------------------------------------------------
+%   DESCRIPTION
+%       [x,lam] = eigsol(K,varargin)
+%       Wrapper for built-in eig() that forces eigenvector scaling   
+%
+%   VERSION
+%       v1.1 / 16.12.21 / V.Yotov
+%  ------------------------------------------------------------------------------------------------
 
 function [x,lam] = eigsol(K,varargin)
-	n = size(K,1);                                                          % No verification for square matrices
 
-	if nargin == 1          [x,lam] = eig(K);                               % Switch between ordinary/full eigenv. problem
+    n = size(K,1);                                                          % No verification for square matrices
+
+    if nargin == 1          [x,lam] = eig(K);                               % Switch between ordinary/full eigenv. problem
     elseif nargin == 2      [x,lam] = eig(K,varargin{1});                   % Same as eig(K,M)
     else                    [x,lam] = eigs(K,varargin{:});                  % Call with (K,M,nEigs,'opts') or (K,nEigs,'opts')
     end
 
-	[x,lam] = eigscale(K,x,lam);
-end
+    [x,lam] = eigscale(K,x,lam);
